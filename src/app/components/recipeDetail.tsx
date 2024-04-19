5e79db63909a4af049fc662ea0943dd0f4330394
Misato <rexlove1226@gmail.com>
2024-04-19 01:57:06 -0700

    fix styling

--- src/app/components/recipeDetail.tsx
+++ src/app/components/recipeDetail.tsx
@@ -12,16 +12,17 @@ const RecipeDetail = ({
 
   return (
     <div>
-      <h1>{recipeDetail.title}</h1>
+      <h1 className="text-2xl">{recipeDetail.title}</h1>
       <Image
         src={recipeDetail.image}
-        width={100}
-        height={50}
+        width={500}
+        height={400}
         alt="recipe image"
       />
       <p>Ready In Minutes: {recipeDetail.readyInMinutes}</p>
       <a href={recipeDetail.spoonacularSourceUrl}>
-        More Detail: {recipeDetail.spoonacularSourceUrl}
+        More Detail:{" "}
+        <span className="underline">{recipeDetail.spoonacularSourceUrl}</span>
       </a>
     </div>
   );
