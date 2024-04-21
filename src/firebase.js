f3bea99bf5ae81dbecdec8a360621019b8d31c55
Misato <rexlove1226@gmail.com>
2024-04-20 17:28:59 -0700

    fix add button

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,44 @@
-import { db } from './firebaseConfig'; // Asegúrate de que importas db correctamente de tu configuración de Firebase
+import { db } from "./firebaseConfig"; // Asegúrate de que importas db correctamente de tu configuración de Firebase
+/*
+export const addRecipe = async (recipe) => {
+  try {
+    await setDoc(doc(db, "Recipes"), {
+      title: recipe.title,
+    });
+
+   
+    const recipeRef = await db.collection("Recipes").add({
+      title: recipe.title,
+      
+      description:
+        "A classic Italian pizza with tomatoes, mozzarella cheese, and fresh basil.",
+      steps: ["Knead the dough", "Add toppings", "Bake in oven"],
+      ingredients: [
+        { name: "Flour", amount: "200g" },
+        { name: "Tomatoes", amount: "100g" },
+      ],
+      image: recipe.image,
+      
+    });
+    
+    console.log("Document written with ID: ", recipeRef.id);
+    return recipeRef.id; // Devolver el ID puede ser útil para operaciones posteriores
+  } catch (error) {
+    console.error("Error adding document: ", error);
+    throw error; // Es bueno re-lanzar el error para manejarlo más arriba si es necesario
+  }
+};
+*/
+
+import { collection, addDoc } from "firebase/firestore";
 
 export const addRecipe = async (recipe) => {
   try {
-      const recipeRef = await db.collection('Recipes').add({
-          title: recipe.title,
-          description: "A classic Italian pizza with tomatoes, mozzarella cheese, and fresh basil.",
-          steps: ["Knead the dough", "Add toppings", "Bake in oven"],
-          ingredients: [
-              {name: "Flour", amount: "200g"},
-              {name: "Tomatoes", amount: "100g"}
-          ],
-          image: recipe.image
-      });
-      console.log("Document written with ID: ", recipeRef.id);
-      return recipeRef.id;  // Devolver el ID puede ser útil para operaciones posteriores
-  } catch (error) {
-      console.error("Error adding document: ", error);
-      throw error;  // Es bueno re-lanzar el error para manejarlo más arriba si es necesario
+    const docRef = await addDoc(collection(db, "Recipes"), {
+      title: recipe.title,
+    });
+    console.log("Document written with ID: ", docRef.id);
+  } catch (e) {
+    console.error("Error adding document: ", e);
   }
 };
--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,6 +1,25 @@
+export type Recipe = {
+  id: number;
+  title: string;
+  image: string;
+  imageType: string;
+};
+
 export type RecipeDetailType = {
   title: string;
   image: string;
   readyInMinutes: number;
   spoonacularSourceUrl: string;
+  extendedIngredients: [
+    {
+      measures: {
+        metric: {
+          amount: number;
+          unitLong: string;
+        };
+      };
+      meta: [];
+      name: string;
+    }
+  ];
 };
