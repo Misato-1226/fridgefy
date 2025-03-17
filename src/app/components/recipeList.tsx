bc0968a89e3f74593ecda6ebdc1daf0d2fe37969
Misato <rexlove1226@gmail.com>
2025-03-17 23:59:30 +0900

    fix styling / function for showing more recipes on recipeList

--- src/app/components/recipeList.tsx
+++ src/app/components/recipeList.tsx
@@ -2,6 +2,7 @@ import { useContext, useEffect, useState } from "react";
 import RecipeItem from "./recipeItem";
 import { RecipeResultContext } from "@/app/contexts/RecipeResult";
 import { Recipe } from "@/lib/types";
+import Image from "next/image";
 
 const RecipeList = () => {
   const recipe = useContext(RecipeResultContext);
@@ -20,7 +21,7 @@ const RecipeList = () => {
   };
 
   const handleBack = () => {
-    if (recipe?.Recipes && loadIndex < recipe?.Recipes.length) {
+    if (loadIndex === 0) {
       setIsBackEmpty(true);
     } else {
       setLoadIndex((prev) => prev - 10);
@@ -37,8 +38,28 @@ const RecipeList = () => {
         )}
       </ul>
       <div className="flex justify-center p-3 gap-x-10 text-xl">
-        <button onClick={handleBack}>←</button>
-        <button onClick={handleNext}>→</button>
+        {!isBackEmpty && (
+          <button
+            onClick={handleBack}
+            className="font-semibold text-6xl text-lime-300 hover:text-lime-400"
+          >
+            ←<span className="text-xl font-normal">戻る</span>
+          </button>
+        )}
+        {!isNextEmpty && (
+          <button
+            onClick={handleNext}
+            className="font-semibold text-6xl text-lime-300 hover:text-lime-400"
+          >
+            <span className="text-xl font-normal">Next</span>→
+            {/* <Image
+            alt="次へ"
+            src="https://img.icons8.com/?size=100&id=39777&format=png&color=bef264"
+            width={50}
+            height={50}
+          /> */}
+          </button>
+        )}
       </div>
     </>
   );
